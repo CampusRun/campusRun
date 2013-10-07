f3261d34538c5d0df87c18a3258fc9e2572997c6
unknown <otten@Otten-PC.connectiv.local>
2013-10-08 01:53:35 +0200

    Menue

--- index.php
+++ index.php
@@ -6,17 +6,19 @@
 <link rel="stylesheet" href="assets/css/jquery.mobile-1.3.2.min.css" />
 <link rel="stylesheet" href="assets/css/start.css" />
 
-
-
-<script type="text/javascript" src="assets/js/jquery-1.9.1.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery.mobile-1.3.2.min.js"></script>
+<script type="text/javascript" src="assets/js/menue/jquery-1.8.1.min.js"></script>
+<script type="text/javascript" src="assets/js/menue/jquery.mobile-1.3.2.min.js"></script>
 
 </head> 
 <body>
-	<h1>Campus Run</h1>
-	<h2>0.1</h2>
-	<a href="game.php" rel="external"data-role="button" data-icon="check" data-theme="b">START</a>
-	<a href="" data-role="button" data-icon="star" data-theme="b">Highscore</a>
-	<a href="" data-role="button" data-icon="icon" data-theme="b">Impressum</a>
+	<div style="width:90%;margin:0px auto;">
+	<div style="text-align:center;">
+	<h1><img src="assets\img\player.gif" width="32" height="48" />Campus Run<img src="assets\img\player.gif" width="32" height="48" /></h1>
+	<h2>v 0.1 a</h2>
+	</div>
+	<a href="game.php" rel="external" data-role="button" data-icon="check" data-theme="a">START</a>
+	<a href="xml/player.xml" rel="external" data-role="button" data-icon="star" data-theme="a">Highscore</a>
+	<a href="" data-role="button" data-icon="icon" data-theme="a">Impressum</a>
+	<div>
 </body>
 </html>
--- xml/playerlist.xsl
+++ xml/playerlist.xsl
@@ -5,10 +5,18 @@
         <html>
             <head>
                 <title>Playerlist</title>
+                <link rel="stylesheet" type="text/css" href="../assets/css/jquery.mobile-1.3.2.min.css"></link>
+                <script type="text/javascript" src="../assets/js/menue/jquery-1.8.1.min.js"></script>
+                <script type="text/javascript" src="../assets/js/menue/jquery.mobile-1.3.2.min.js"></script>
+                
             </head>
             <body>
+                <div style="width:90%;margin:0px auto;">
+                <div data-role="header"> 
                 <h1>Playerliste</h1>
-                <table border="1">
+                </div>
+                <div style="margin:20px auto;width:250px;">
+                <table border="1" width="250px">
                 <tr bgcolor="#9acd32">
                   <th>Playername</th>
                   <th>Time</th>
@@ -24,6 +32,11 @@
                     </tr>
                 </xsl:for-each>
                 </table>
+                </div>
+                <div data-role="footer" class="ui-bar">
+                  <a href="http://campusrun.connectiv.info/index.php" data-role="button" data-icon="arrow-l">Zurück</a>
+                </div>
+                </div>
             </body>
         </html>
     </xsl:template>
